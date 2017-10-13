fd4f51838bda9e7406fe5c48fa2d92403e7c7ba8
S0N <S0N-0F-A-S0N@users.noreply.github.com>
2017-10-12 19:34:08 -0500

    √

--- main.py
+++ main.py
@@ -33,6 +33,12 @@ def blog():
 def about():
     return render_template('about.html',title="About page")
 
+@app.route("/blogpost", methods=['GET'])
+def blogpost():
+    title = request.args['title']
+    body = request.args['body']
+    return render_template('blogpost.html', title=title, body=body)
+
 @app.route("/posted", methods=['POST', 'GET'])
 def posted():
     if request.method == 'POST':
@@ -50,7 +56,8 @@ def posted():
             new_post = Blog(title, body)
             db.session.add(new_post)
             db.session.commit()
-            return render_template('posted.html', title="Content posted")
+            return render_template('blogpost.html', title=title, body=body)
+            #return render_template('posted.html', title="Content posted")
         else:
             return render_template('post.html', title="Content not posted", error1 = title_error, error2 = body_error)
     return render_template('notposted.html', title="Content not posted")
